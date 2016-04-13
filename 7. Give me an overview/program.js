b6069a7f1c603bc0ee73b4ddb6126e360ce482cd
Pavle <V381@users.noreply.github.com>
2016-04-13 17:42:35 +0200

    Update program.js

--- 7. Give me an overview/program.js
+++ 7. Give me an overview/program.js
@@ -1,7 +1,22 @@
 var _ = require('lodash');
 
 var worker = function(list){
-	
+	var hm = {}, score = 0, qwert = [];
+	var obj = _.groupBy(list, 'article');
+	_.forEach(obj, function(value, key){
+	    _.reduce(value, function(all, item, i){
+	          all += item.quantity
+	          return hm[item.article] = all
+	    }, 0);
+	});
+	_.forEach(hm, function(key, value){
+	    qwert.push(
+		    {'article': Number(value),
+		     'total_orders' : key
+		    }
+	    )
+	});
+	return _.sortBy(qwert, "total_orders").reverse();
 }
 
 module.exports = worker;
